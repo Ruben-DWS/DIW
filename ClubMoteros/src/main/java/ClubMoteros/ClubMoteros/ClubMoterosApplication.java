b83c24aeedeccd0910e11d4071dbfffe54ad8aef
Ruben-DWS <csi24-rgarugo@colegioaltair.net>
2024-11-07 14:14:51 +0100

    Proyecto Nuevo

---
+++ ClubMoteros/src/main/java/ClubMoteros/ClubMoteros/ClubMoterosApplication.java
@@ -0,0 +1,13 @@
+package ClubMoteros.ClubMoteros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMoterosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubMoterosApplication.class, args);
+	}
+
+}
---
+++ ClubMoteros/src/test/java/ClubMoteros/ClubMoteros/ClubMoterosApplicationTests.java
@@ -0,0 +1,13 @@
+package ClubMoteros.ClubMoteros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubMoterosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
